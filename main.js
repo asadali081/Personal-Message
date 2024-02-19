be94db47976191af2be6d746f12012b915cd4780
Asad <159769658+asadali081@users.noreply.github.com>
2024-02-19 11:35:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var x = ("Asad");
+console.log("Hello", x + ", Would you like to learn Python Today?");
---
+++ main.ts
@@ -0,0 +1,2 @@
+let x = ("Asad") 
+console .log ("Hello",x +", Would you like to learn Python Today?")
